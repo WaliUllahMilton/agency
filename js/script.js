232c28c0ba99ba365981eb77e74c883bb0afdba3
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-08-04 20:18:07 +0600

    final responsive update

--- js/script.js
+++ js/script.js
@@ -6,19 +6,20 @@ $('.slider-part').slick({
     nextArrow:'<i><i class="fa-solid fa-arrow-right right"></i></i>',
     responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 992,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 1,
             infinite: true,
-            dots: true
+            arrows:false
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 768,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false
           }
         },
         {
