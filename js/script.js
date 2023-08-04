4bc16632cb2ae55c8cb78889956ca41b9a5537ea
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-08-04 17:43:42 +0600

    responsive update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,35 @@ $('.slider-part').slick({
     arrows:true,
     prevArrow:'<i><i class="fa-solid fa-arrow-left left"></i></i>',
     nextArrow:'<i><i class="fa-solid fa-arrow-right right"></i></i>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 $('.slider').slick({
     infinite:false,
